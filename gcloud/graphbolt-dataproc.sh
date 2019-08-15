bbe98676c580266292f06266077a66d2dabc2222
Miguel Coimbra <mcoimbra@users.noreply.github.com>
2019-08-15 12:58:46 +0100

    Editing Google DataProc image creation script.

--- gcloud/graphbolt-dataproc.sh
+++ gcloud/graphbolt-dataproc.sh
@@ -29,29 +29,27 @@ mkdir -p $GRAPHBOLT_ROOT/datasets/web/
 mkdir -p $GRAPHBOLT_ROOT/datasets/social/
 
 GS_BUCKET="graphbolt-bucket"
-GS_BUCKET_DATASETS_DIR="/datasets"
+GS_BUCKET_DATASETS_DIR="$GS_BUCKET/datasets"
 
 mkdir -p $GRAPHBOLT_ROOT/datasets/web/eu-2005-40000-random
-gsutil cp -r gs://$GS_BUCKET$GS_BUCKET_DATASETS_DIR/web/eu-2005-40000-random/* /home/GraphBolt/datasets/web/eu-2005-40000-random/
+gsutil cp -r gs://$GS_BUCKET_DATASETS_DIR/web/eu-2005-40000-random/* /home/GraphBolt/datasets/web/eu-2005-40000-random/
 
 mkdir -p $GRAPHBOLT_ROOT/datasets/social/amazon-2008-40000-random
-gsutil cp -r gs://$GS_BUCKET$GS_BUCKET_DATASETS_DIR/social/amazon-2008-40000-random/* /home/GraphBolt/datasets/social/amazon-2008-40000-random/
+gsutil cp -r gs://$GS_BUCKET_DATASETS_DIR/social/amazon-2008-40000-random/* /home/GraphBolt/datasets/social/amazon-2008-40000-random/
 
 ## Create and copy the GraphBolt code directories.
 GRAPHBOLT_CODE_DIR=$GRAPHBOLT_ROOT/Projects/GraphBolt
 mkdir -p $GRAPHBOLT_CODE_DIR
 
-GS_BUCKET_CODE_DIR="GraphBolt"
-gsutil cp -r gs://$GS_BUCKET/$GS_BUCKET_CODE_DIR/python $GRAPHBOLT_CODE_DIR/
-gsutil cp -r gs://$GS_BUCKET/$GS_BUCKET_CODE_DIR/src $GRAPHBOLT_CODE_DIR/
-gsutil cp gs://$GS_BUCKET/$GS_BUCKET_CODE_DIR/pom.xml $GRAPHBOLT_CODE_DIR/
-gsutil cp gs://$GS_BUCKET/$GS_BUCKET_CODE_DIR/README.md $GRAPHBOLT_CODE_DIR/
-gsutil cp gs://$GS_BUCKET/$GS_BUCKET_CODE_DIR/LICENSE-2.0.txt $GRAPHBOLT_CODE_DIR/
-
-#TODO TODO TODO: add mvn to $PATH
+GS_BUCKET_CODE_DIR="$GS_BUCKET/GraphBolt"
+gsutil cp -r gs://$GS_BUCKET_CODE_DIR/python $GRAPHBOLT_CODE_DIR/
+gsutil cp -r gs://$GS_BUCKET_CODE_DIR/src $GRAPHBOLT_CODE_DIR/
+gsutil cp gs://$GS_BUCKET_CODE_DIR/pom.xml $GRAPHBOLT_CODE_DIR/
+gsutil cp gs://$GS_BUCKET_CODE_DIR/README.md $GRAPHBOLT_CODE_DIR/
+gsutil cp gs://$GS_BUCKET_CODE_DIR/LICENSE-2.0.txt $GRAPHBOLT_CODE_DIR/
 
 #cd $GRAPHBOLT_CODE_DIR
-#mvn clean install
+#/usr/bin/mvn clean install
 
 
 
