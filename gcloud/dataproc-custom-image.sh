69d7ce472d3a86f03d261b950697c3e1ed533ee0
Miguel Coimbra <mcoimbra@users.noreply.github.com>
2019-10-10 18:39:55 +0100

    Minor update to image generation script.

--- gcloud/dataproc-custom-image.sh
+++ gcloud/dataproc-custom-image.sh
@@ -73,31 +73,31 @@ unzip $GS_GRAPHBOLT_ZIP_NAME
 # Prepare .bash_profile and misc utilities.
 sudo touch ${GRAPHBOLT_ROOT}/.bash_profile
 sudo cat <<EOF >>${GRAPHBOLT_ROOT}/.bash_profile
-SSH_ENV="$HOME/.ssh/environment"
+SSH_ENV="\$HOME/.ssh/environment"
 
 function start_agent {
     echo "Initialising new SSH agent..."
-    /usr/bin/ssh-agent | sed 's/^echo/#echo/' > "$SSH_ENV"
+    /usr/bin/ssh-agent | sed 's/^echo/#echo/' > "\$SSH_ENV"
     echo succeeded
-    chmod 600 "${SSH_ENV}"
-    . "${SSH_ENV}" > /dev/null
+    chmod 600 "\${SSH_ENV}"
+    . "\${SSH_ENV}" > /dev/null
     /usr/bin/ssh-add;
 }
 
 # Source SSH settings, if applicable
 
-if [ -f "${SSH_ENV}" ]; then
-    . "${SSH_ENV}" > /dev/null
-    #ps ${SSH_AGENT_PID} doesn't work under cywgin
-    ps -ef | grep ${SSH_AGENT_PID} | grep ssh-agent$ > /dev/null || {
+if [ -f "\${SSH_ENV}" ]; then
+    . "\${SSH_ENV}" > /dev/null
+    #ps \${SSH_AGENT_PID} doesn't work under cywgin
+    ps -ef | grep \${SSH_AGENT_PID} | grep ssh-agent$ > /dev/null || {
         start_agent;
     }
 else
     start_agent;
 fi
 
-ssh-add $HOME/.ssh/$CLUSTER_SSH_PKEY
-ssh-add $HOME/.ssh/$GITHUB_SSH_PKEY
+ssh-add \$HOME/.ssh/\$CLUSTER_SSH_PKEY
+ssh-add \$HOME/.ssh/\$GITHUB_SSH_PKEY
 
 EOF
 
