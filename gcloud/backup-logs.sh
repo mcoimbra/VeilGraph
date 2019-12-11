dffbbc5873147da69ed186a3411f2b5d37189602
Miguel Coimbra <mcoimbra@users.noreply.github.com>
2019-12-11 13:57:36 +0000

    Added log backup script.

--- gcloud/backup-logs.sh
+++ gcloud/backup-logs.sh
@@ -5,13 +5,14 @@
 PARALLELISM=$(/usr/share/google/get_metadata_value attributes/dataproc-worker-count)
 
 DATE_TIME_STAMP=$(printf -v date '%(%Y-%m-%d %H:%M:%S)T\n' -1)
+printf "> Timestamp:\t$DATE_TIME_STAMP\n"
 DATE_TIME_STAMP=$(echo $date | tr ' ' _ )
 
 GCLOUD_DIR_NAME="logs_$DATE_TIME_STAMP"
 
 GCLOUD_PATH="gs://graphbolt-storage/testing/flink_logs/$GCLOUD_DIR_NAME"
 
-printf "> Timestamp:\t$DATE_TIME_STAMP"
+printf "> Timestamp:\t$DATE_TIME_STAMP\n"
 
 printf "> Copying log files to $GCLOUD_PATH\n"
 
