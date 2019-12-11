130c20920e7dfbe52a0420e40ffa07fbddb33725
Miguel Coimbra <mcoimbra@users.noreply.github.com>
2019-12-11 13:53:01 +0000

    Added log backup script.

--- gcloud/backup-logs.sh
+++ gcloud/backup-logs.sh
@@ -9,11 +9,11 @@ DATE_TIME_STAMP=$(echo $date | tr ' ' _ )
 
 GCLOUD_DIR_NAME="logs_$DATE_TIME_STAMP"
 
-GCLOUD_PATH='gs://graphbolt-storage/testing/flink_logs/$GCLOUD_DIR_NAME'
+GCLOUD_PATH="gs://graphbolt-storage/testing/flink_logs/$GCLOUD_DIR_NAME"
 
 echo "> Copying log files to $GCLOUD_PATH"
 
 for (( i = 0; i < PARALLELISM; ++i )); do
     echo $i
-    #ssh "graphbolt@graphbolt-cluster-w-$i" 'gsutil cp -p /usr/lib/flink/log/* $GCLOUD_PATH'
+    ssh "graphbolt@graphbolt-cluster-w-$i" "gsutil cp -p /usr/lib/flink/log/* $GCLOUD_PATH"
 done
