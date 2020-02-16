78df2c8829722fb829087015cfc1bdae01ef113e
Miguel Coimbra <mcoimbra@users.noreply.github.com>
2020-02-16 23:48:46 +0000

    misc

--- gcloud/test-commands.sh
+++ gcloud/test-commands.sh
@@ -9,6 +9,7 @@ function run_complete() {
   local cluster_parallelism=$2
   local dataset_dir=$3
   local dataset_name=$4
+  local region=$5
   
   rbo_length=$5
   pagerank_iterations=30
@@ -21,15 +22,15 @@ function run_complete() {
 
   #return
 
-  #gcloud dataproc clusters create $cluster_name --region us-east1 --image graphbolt-debian --zone us-east1-b --initialization-actions gs://graphbolt-storage/dataproc-initialization-actions/flink/flink.sh --num-workers $cluster_parallelism --master-machine-type custom-4-16384 --worker-machine-type custom-4-16384
+  #gcloud dataproc clusters create $cluster_name --region $region --image graphbolt-debian --zone $region-b --initialization-actions gs://graphbolt-storage/dataproc-initialization-actions/flink/flink.sh --num-workers $cluster_parallelism --master-machine-type custom-4-16384 --worker-machine-type custom-4-16384
 
-  gcloud dataproc clusters create $cluster_name --region us-east1 --image graphbolt-debian --zone us-east1-b --initialization-actions gs://graphbolt-storage/dataproc-initialization-actions/flink/flink.sh --num-workers $cluster_parallelism --master-machine-type custom-4-16384 --worker-machine-type custom-4-26368
+  gcloud dataproc clusters create $cluster_name --region $region --image graphbolt-debian --zone $region-b --initialization-actions gs://graphbolt-storage/dataproc-initialization-actions/flink/flink.sh --num-workers $cluster_parallelism --master-machine-type custom-4-16384 --worker-machine-type custom-4-26368
   
-  gcloud beta compute --project "datastorm-1083" ssh --zone "us-east1-b" "graphbolt@$cluster_name-m" --command 'cd /home/graphbolt/Documents/Projects/GraphBolt.git/python && python3 -m graphbolt.algorithm.randomwalk.pagerank.run -delete-edges -i '$dataset_name' -chunks 50 -out-dir "/home/graphbolt/Documents/Projects/GraphBolt.git/testing" -data-dir '$dataset_dir' -cache "gs://graphbolt-storage/cache" -p '$flink_parallelism' -size '$rbo_length' -iterations '$pagerank_iterations' -damp '$damp' -periodic-full-dump -temp "/home/graphbolt/Documents/Projects/GraphBolt.git/testing/Temp" -flink-address '$cluster_name-m' -flink-port 8081'
+  gcloud beta compute --project "datastorm-1083" ssh --zone "$region-b" "graphbolt@$cluster_name-m" --command 'cd /home/graphbolt/Documents/Projects/GraphBolt.git/python && python3 -m graphbolt.algorithm.randomwalk.pagerank.run -delete-edges -i '$dataset_name' -chunks 50 -out-dir "/home/graphbolt/Documents/Projects/GraphBolt.git/testing" -data-dir '$dataset_dir' -cache "gs://graphbolt-storage/cache" -p '$flink_parallelism' -size '$rbo_length' -iterations '$pagerank_iterations' -damp '$damp' -periodic-full-dump -temp "/home/graphbolt/Documents/Projects/GraphBolt.git/testing/Temp" -flink-address '$cluster_name-m' -flink-port 8081'
   
-  gcloud beta compute --project "datastorm-1083" ssh --zone "us-east1-b" "graphbolt@$cluster_name-m" --command 'source /home/graphbolt/.bash_profile && ssh-add /home/graphbolt/.ssh/cluster && cd /home/graphbolt/Documents/Projects/GraphBolt.git/gcloud && ./backup-logs.sh '$dataset_name'_'$pagerank_iterations'_'$rbo_length'_P'$flink_parallelism'_'$damp'_complete'
+  gcloud beta compute --project "datastorm-1083" ssh --zone "$region-b" "graphbolt@$cluster_name-m" --command 'source /home/graphbolt/.bash_profile && ssh-add /home/graphbolt/.ssh/cluster && cd /home/graphbolt/Documents/Projects/GraphBolt.git/gcloud && ./backup-logs.sh '$dataset_name'_'$pagerank_iterations'_'$rbo_length'_P'$flink_parallelism'_'$damp'_complete'
 
-  echo "Y" | gcloud dataproc clusters delete $cluster_name --region=us-east1
+  echo "Y" | gcloud dataproc clusters delete $cluster_name --region=$region
 }
 
 function run_summarized() {
@@ -41,6 +42,7 @@ function run_summarized() {
   local n_param=$6
   local delta_param=$7
   local rbo_length=$8
+  local region=$9
   
   
   
@@ -55,17 +57,17 @@ function run_summarized() {
 
   #return
 
-  #gcloud dataproc clusters create $cluster_name --region us-east1 --image graphbolt-debian --zone us-east1-b --initialization-actions gs://graphbolt-storage/dataproc-initialization-actions/flink/flink.sh --num-workers $cluster_parallelism --master-machine-type custom-4-16384 --worker-machine-type custom-4-16384
+  #gcloud dataproc clusters create $cluster_name --region $region --image graphbolt-debian --zone $region-b --initialization-actions gs://graphbolt-storage/dataproc-initialization-actions/flink/flink.sh --num-workers $cluster_parallelism --master-machine-type custom-4-16384 --worker-machine-type custom-4-16384
 
-  gcloud dataproc clusters create $cluster_name --region us-east1 --image graphbolt-debian --zone us-east1-b --initialization-actions gs://graphbolt-storage/dataproc-initialization-actions/flink/flink.sh --num-workers $cluster_parallelism --master-machine-type custom-4-16384 --worker-machine-type custom-4-26368
+  gcloud dataproc clusters create $cluster_name --region $region --image graphbolt-debian --zone $region-b --initialization-actions gs://graphbolt-storage/dataproc-initialization-actions/flink/flink.sh --num-workers $cluster_parallelism --master-machine-type custom-4-16384 --worker-machine-type custom-4-26368
   
-  gcloud beta compute --project "datastorm-1083" ssh --zone "us-east1-b" "graphbolt@$cluster_name-m" --command 'cd /home/graphbolt/Documents/Projects/GraphBolt.git/python && python3 -m graphbolt.algorithm.randomwalk.pagerank.run -delete-edges -i '$dataset_name' -chunks 50 -out-dir "/home/graphbolt/Documents/Projects/GraphBolt.git/testing" -data-dir '$dataset_dir' -cache "gs://graphbolt-storage/cache" -p '$flink_parallelism' -size '$rbo_length' -iterations '$pagerank_iterations' -damp '$damp' -periodic-full-dump -temp "/home/graphbolt/Documents/Projects/GraphBolt.git/testing/Temp" -flink-address '$cluster_name'-m -flink-port 8081 -summarized-only -l '$r_param' '$n_param' '$delta_param''
+  gcloud beta compute --project "datastorm-1083" ssh --zone "$region-b" "graphbolt@$cluster_name-m" --command 'cd /home/graphbolt/Documents/Projects/GraphBolt.git/python && python3 -m graphbolt.algorithm.randomwalk.pagerank.run -delete-edges -i '$dataset_name' -chunks 50 -out-dir "/home/graphbolt/Documents/Projects/GraphBolt.git/testing" -data-dir '$dataset_dir' -cache "gs://graphbolt-storage/cache" -p '$flink_parallelism' -size '$rbo_length' -iterations '$pagerank_iterations' -damp '$damp' -periodic-full-dump -temp "/home/graphbolt/Documents/Projects/GraphBolt.git/testing/Temp" -flink-address '$cluster_name'-m -flink-port 8081 -summarized-only -l '$r_param' '$n_param' '$delta_param''
   
-  gcloud beta compute --project "datastorm-1083" ssh --zone "us-east1-b" "graphbolt@$cluster_name-m" --command 'source /home/graphbolt/.bash_profile && ssh-add /home/graphbolt/.ssh/cluster && cd /home/graphbolt/Documents/Projects/GraphBolt.git/gcloud && ./backup-logs.sh '$dataset_name'_'$pagerank_iterations'_'$rbo_length'_P'$flink_parallelism'_'$damp'_model_'$r_param'_'$n_param'_'$delta_param''
+  gcloud beta compute --project "datastorm-1083" ssh --zone "$region-b" "graphbolt@$cluster_name-m" --command 'source /home/graphbolt/.bash_profile && ssh-add /home/graphbolt/.ssh/cluster && cd /home/graphbolt/Documents/Projects/GraphBolt.git/gcloud && ./backup-logs.sh '$dataset_name'_'$pagerank_iterations'_'$rbo_length'_P'$flink_parallelism'_'$damp'_model_'$r_param'_'$n_param'_'$delta_param''
   
 
 
-  echo "Y" | gcloud dataproc clusters delete $cluster_name --region=us-east1
+  echo "Y" | gcloud dataproc clusters delete $cluster_name --region=$region
 
 }
 
@@ -81,18 +83,19 @@ main() {
 	N=$4
 	DELTA=$5
   RBO_LEN=$6
+  REGION=$7
 
   
-  #run_complete 1 2 $DATA_DIR $DATASET_PREFIX $RBO_LEN &
+  #run_complete 1 2 $DATA_DIR $DATASET_PREFIX $RBO_LEN $REGION &
 
-  #run_summarized 1 2 $DATA_DIR $DATASET_PREFIX $R $N $DELTA $RBO_LEN &
+  #run_summarized 1 2 $DATA_DIR $DATASET_PREFIX $R $N $DELTA $RBO_LEN $REGION &
 
   #for d in 2 4 8 16; do
   #for d in 4 8 16; do
-  for d in 4; do
-    run_complete $d $d $DATA_DIR $DATASET_PREFIX $RBO_LEN &
+  for d in 8 16; do
+    run_complete $d $d $DATA_DIR $DATASET_PREFIX $RBO_LEN $REGION &
 
-    run_summarized $d $d $DATA_DIR $DATASET_PREFIX $R $N $DELTA $RBO_LEN &
+    run_summarized $d $d $DATA_DIR $DATASET_PREFIX $R $N $DELTA $RBO_LEN $REGION &
   done
 
   wait
